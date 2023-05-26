1467d02c5777cc14351cb1763c37d006517283d7
Nestor Martinez <nesturron@gmail.com>
2023-05-26 09:17:53 +0200

    Lists controller done, bookmarks controller with new and create, basic styling done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,28 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: %i[new create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = Movie.find(params[:movie_id])
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,23 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root to: 'lists#index'
+
+  # # Index lists
+  # get '/lists', to: 'lists#index'
+
+  # # New list
+  # get '/lists/new', to: 'lists#new', as: :new_list
+  # post '/lists', to: 'lists#create'
+
+  # # Show list
+  # get '/lists/:id', to: 'lists#show', as: :list
+
+  # # New Bookmark
+  # get '/lists/:list_id/bookmarks/new', to: 'bookmarks#new', as: :new_list_bookmark
+  # post '/lists/:id', to: 'bookmarks#create'
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
